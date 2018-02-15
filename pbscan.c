3110230c47f5f8566d33702904113021353418ae
Tuomas Hämälä <tuomas.hamala@gmail.com>
2018-02-15 16:05:15 -0600

    Version 2018.02.15

--- pbscan.c
+++ pbscan.c
@@ -2,8 +2,19 @@
  
  PBScan, program for performing selection outlier scans with population branch statistic (PBS)
  
- Code written by Tuomas Hamala
- tuomas.hamala@gmail.com
+ Copyright (C) 2017 Tuomas Hamala
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ For any other enquiries send an email to tuomas.hamala@gmail.com
  
  Manual available at: https://github.com/thamala/PBScan
  
