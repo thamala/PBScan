5ca79054e8eb62671433e30d6e681afbbff05106
Tuomas Hämälä <tuomas.hamala@gmail.com>
2019-01-15 10:33:33 -0600

    Version 2018.02.15

--- pbscan.c
+++ pbscan.c
@@ -1912,5 +1912,5 @@ void showHelp(void){
     puts("\t-min [int]\tMinimum number of individuals required per population (default is 1)");
     puts("\t-maf [double]\tMinimum minor allele frequency required per site (default is 0.01)");
     puts("\nUsage example:");
-    puts("./pbscan -likes example.likes -pop1 list1.txt -pop2 list2.txt -pop3 list3.txt -out output -win 50 -step 50 -ms example.ms -div 2 -min 5 -maf 0.05\n");
+    puts("./pbscan -likes example.likes -pop1 list1.txt -pop2 list2.txt -pop3 list3.txt -out output -win 50 -step 51 -ms example.ms -div 2 -min 5 -maf 0.05\n");
 }
